f6d94a5132b52824388ab47aefea166476c00aa0
arari3s <arlanariandi@gmail.com>
2023-04-10 22:31:15 +0700

    first commit add component HeaderComponent

---
+++ src/views/HomeView.vue
@@ -0,0 +1,17 @@
+<template>
+    <div class="home">
+        <HeaderCompnent />
+    </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import HeaderCompnent from '@/components/HeaderComponent.vue'
+
+export default {
+    // name: 'HeaderCompnent',
+    components: {
+        HeaderCompnent
+    }
+}
+</script>
